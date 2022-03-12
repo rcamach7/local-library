ce8176ca89731e8549068b78c3e4ece080bad9df
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-03-12 14:02:25 -0800

    implement delete endpoints for book instances

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -18,13 +18,13 @@ exports.bookinstance_list = function (req, res, next) {
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = function (req, res, next) {
-  console.log(req.params);
   BookInstance.findById(req.params.id)
     .populate('book')
     .exec((err, bookinstance) => {
       if (err) next(err);
+
       if (bookinstance === null) {
-        const err = new Error('Book copy not found');
+        const err = new Error('Book Instance copy not found');
         err.status = 404;
         return next(err);
       }
@@ -95,13 +95,34 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+// Passes in ID - and we only provide bookinstance information to the delete page
+exports.bookinstance_delete_get = function (req, res, next) {
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec((err, bookinstance) => {
+      if (err) next(err);
+
+      if (bookinstance === null) {
+        const error = new Error('Book Instance not found');
+        error.status = 404;
+
+        return next(err);
+      }
+
+      res.render('bookinstance_delete', {
+        title: 'Delete Book-Instance',
+        bookinstance: bookinstance,
+      });
+    });
 };
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+  BookInstance.findByIdAndDelete(req.params.id, (err) => {
+    if (err) next(err);
+
+    res.redirect('/catalog/bookinstances');
+  });
 };
 
 // Display BookInstance update form on GET.
